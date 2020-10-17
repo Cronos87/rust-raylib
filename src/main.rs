13a73bb80cb7b2c533cb30362789e236517989c7
Cronos87 <cronos87@hotmail.fr>
2020-10-17 19:47:37 +0200

    Added init method

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,42 @@ fn main() {
     // Prevent to quit the game pressing escape
     rl.set_exit_key(None);
 
+    // Create the first scene of the game
     let main_scene = MainScene::new();
+    main_scene.init(&mut rl);
+
+    // Define the current scene as the first scene
     let mut current_scene: Box<dyn Scene> = Box::new(main_scene);
+
+    // Store the next scene
+    let mut next_scene: Option<Box<dyn Scene>> = None;
 
     // Main game loop
     while !rl.window_should_close() {
+        /* --------------------- Init Scene --------------------- */
+        if let Some(scene) = next_scene {
+            current_scene = scene;
+
+            // Init the scene
+            current_scene.init(&mut rl);
+
+            // Reset the next scene
+            next_scene = None
+        }
+
         /* -------------------- Update Scene -------------------- */
         let new_scene = current_scene.update(&mut rl);
+
+        /* ---------------------- New Scene --------------------- */
+        // If a new scene has been returns,
+        // define the next scene with the new one
+        if let Some(scene) = new_scene {
+            // Replace scene
+            current_scene = scene;
+
+            // Init the new scene
+            current_scene.init(&mut rl);
+        }
 
         /* ------------------------ Draw ------------------------ */
         let mut d = rl.begin_drawing(&thread);
@@ -36,10 +65,5 @@ fn main() {
 
         // Draw the current scene
         current_scene.draw(&mut d);
-
-        /* --------------------- Init Scene --------------------- */
-        if let Some(scene) = new_scene {
-            current_scene = scene;
-        }
     }
 }
--- src/scene.rs
+++ src/scene.rs
@@ -6,11 +6,23 @@ use raylib::RaylibHandle;
 
 /// Trait representing game's scene
 pub trait Scene {
+    /// Init the scene. Invoked when starting
+    /// a new scene
+    ///
+    /// # Arguments
+    ///
+    /// * `rl` - Raylib instance
+    fn init(&self, _rl: &mut RaylibHandle) {}
+
     /// Update the scene on every frame
     ///
     /// # Arguments
     ///
     /// * `rl` - Raylib instance
+    ///
+    /// # Returns
+    ///
+    /// Returns a instance of a new scene
     fn update(&mut self, rl: &mut RaylibHandle) -> Option<Box<dyn Scene>>;
 
     /// Draw the scene on every frame
